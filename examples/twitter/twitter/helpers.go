c10900fc491cdb9d10003e6cc8011fde4cf0cf16
Steven Chen <22718607+just-stv@users.noreply.github.com>
2024-12-22 13:59:05 -0800

    More details added

--- examples/twitter/twitter/helpers.go
+++ examples/twitter/twitter/helpers.go
@@ -8,6 +8,7 @@ import (
 	"golang.org/x/exp/rand"
 )
 
+// getRandomInterval returns a random duration between the configured Min and Max intervals
 func (k *Twitter) getRandomInterval() time.Duration {
 	min := k.twitterConfig.MonitorInterval.Min
 	max := k.twitterConfig.MonitorInterval.Max
@@ -26,6 +27,7 @@ func (k *Twitter) getRandomInterval() time.Duration {
 	return time.Duration(randomNanos)
 }
 
+// sleepWithInterrupt waits for the specified duration unless the context is canceled
 func (k *Twitter) sleepWithInterrupt(duration time.Duration) error {
 	k.logger.Infof("Waiting %v until next processing", duration)
 
@@ -37,10 +39,12 @@ func (k *Twitter) sleepWithInterrupt(duration time.Duration) error {
 	}
 }
 
+// isTweetTooOld checks if the tweet's creation time is older than 300 minutes
 func (k *Twitter) isTweetTooOld(tweet *twitter.ParsedTweet) bool {
 	return time.Since(time.Unix(tweet.TweetCreatedAt, 0)) > 300*time.Minute
 }
 
+// isOwnTweet verifies if the provided username matches the authenticated user's name
 func (k *Twitter) isOwnTweet(username string) bool {
 	return strings.ToLower(username) == strings.ToLower(k.twitterConfig.Credentials.User)
 }
