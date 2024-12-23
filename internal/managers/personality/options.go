24607af84a997edf509a62fcfec107c95147b77f
Steven Chen <22718607+just-stv@users.noreply.github.com>
2024-12-22 21:47:24 -0800

    Minor performance update

--- internal/managers/personality/options.go
+++ internal/managers/personality/options.go
@@ -5,6 +5,7 @@ import (
 	"zen/pkg/options"
 )
 
+// ValidateRequiredFields checks if all required fields, including the personality, are set
 func (pm *PersonalityManager) ValidateRequiredFields() error {
 	if err := pm.BaseManager.ValidateRequiredFields(); err != nil {
 		return err
@@ -15,6 +16,7 @@ func (pm *PersonalityManager) ValidateRequiredFields() error {
 	return nil
 }
 
+// WithPersonality returns an Option that sets the Personality on PersonalityManager
 func WithPersonality(personality *Personality) options.Option[PersonalityManager] {
 	return func(m *PersonalityManager) error {
 		m.personality = personality
